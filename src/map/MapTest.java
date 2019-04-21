654bc7be082210625e3c864c8bc7346a0e044f1a
aliouswang <wsw622@126.com>
2019-04-22 07:45:23 +0800

    add map

---
+++ src/map/MapTest.java
@@ -0,0 +1,19 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapTest {
+
+    public static void main(String[] args) {
+        HashMap hashMap;
+        Hashtable hashtable;
+        LinkedHashMap linkedHashMap;
+        ConcurrentHashMap concurrentHashMap;
+        TreeMap treeMap;
+    }
+
+}
