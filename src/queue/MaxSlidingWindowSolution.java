86e16c40aeb9b26e64ef06242c50febf342617a4
aliouswang <wsw622@126.com>
2019-04-30 08:01:17 +0800

    max sliding widnow

--- src/queue/MaxSlidingWindowSolution.java
+++ src/queue/MaxSlidingWindowSolution.java
@@ -12,14 +12,14 @@ public class MaxSlidingWindowSolution {
     public static void main(String[] args) {
         MaxSlidingWindowSolution solution = new MaxSlidingWindowSolution();
 
-//        int[] nums = {1,2,7,7,2,10,3,4,5};
+        int[] nums = {1,2,7,7,2,10,3,4,5};
 //
 //        [1,2,7,7,2,10,3,4,5]
 //        2
 
-        int[] nums = {1,2,7,7,2};
+//        int[] nums = {1,2,7,7,2};
 
-        List<Integer> result = solution.maxSlidingWindow(nums, 3);
+        List<Integer> result = solution.maxSlidingWindow(nums, 2);
 
         Log.d(result.toString());
 
@@ -32,8 +32,49 @@ public class MaxSlidingWindowSolution {
         List<Integer> resultList = new LinkedList<>();
         for (int i = 0; i < nums.length; i++) {
 
-            tryOfferIntoDeque(arrayDeque, nums[i], k, i + 1);
-            if (i >= k - 1) {
+            if (i + 1 < k) {
+                while (!arrayDeque.isEmpty()) {
+                    int first = arrayDeque.peekFirst();
+                    if (first <= nums[i]) {
+                        arrayDeque.pollFirst();
+                    } else {
+                        break;
+                    }
+                }
+                arrayDeque.offer(nums[i]);
+            } else {
+                while (arrayDeque.size() >= k) arrayDeque.pollFirst();
+                // find next max
+                if (arrayDeque.isEmpty()) {
+
+                } else {
+                    Iterator<Integer> integerIterator = arrayDeque.iterator();
+                    int max = integerIterator.next();
+                    int index = 0;
+                    int maxIndex = 0;
+                    while (integerIterator.hasNext()) {
+                        int next = integerIterator.next();
+                        if (max <= next) {
+                            max = next;
+                            maxIndex = index;
+                        }
+                        index++;
+                    }
+                    while (maxIndex > 0) {
+                        arrayDeque.pollFirst();
+                        maxIndex--;
+                    }
+                }
+                while (!arrayDeque.isEmpty()) {
+                    int first = arrayDeque.peekFirst();
+                    if (first <= nums[i]) {
+                        arrayDeque.pollFirst();
+                    } else {
+                        break;
+                    }
+                }
+                arrayDeque.offer(nums[i]);
+
                 resultList.add(arrayDeque.peekFirst());
             }
         }
@@ -41,32 +82,32 @@ public class MaxSlidingWindowSolution {
         return resultList;
     }
 
-    private void tryOfferIntoDeque(ArrayDeque<Integer> arrayDeque, int number, int k, int index) {
-        if (index >= k) {
-            arrayDeque.pollFirst();
-            while (!arrayDeque.isEmpty()) {
-                int first = arrayDeque.peekFirst();
-                if (first <= number) {
-                    arrayDeque.pollFirst();
-                } else {
-                    break;
-                }
-            }
-            arrayDeque.offer(number);
-            return;
-        } else {
-            if (arrayDeque.isEmpty()) {
-                arrayDeque.offer(number);
-                return;
-            }
-            int first = arrayDeque.peekFirst();
-            if (number > first) {
-                arrayDeque.clear();
-                arrayDeque.offer(number);
-            } else {
-                arrayDeque.offer(number);
-            }
-        }
-    }
+//    private void tryOfferIntoDeque(ArrayDeque<Integer> arrayDeque, int number, int k, int index) {
+//        if (index >= k) {
+//            arrayDeque.pollFirst();
+//            while (!arrayDeque.isEmpty()) {
+//                int first = arrayDeque.peekFirst();
+//                if (first <= number) {
+//                    arrayDeque.pollFirst();
+//                } else {
+//                    break;
+//                }
+//            }
+//            arrayDeque.offer(number);
+//            return;
+//        } else {
+//            if (arrayDeque.isEmpty()) {
+//                arrayDeque.offer(number);
+//                return;
+//            }
+//            int first = arrayDeque.peekFirst();
+//            if (number > first) {
+//                arrayDeque.clear();
+//                arrayDeque.offer(number);
+//            } else {
+//                arrayDeque.offer(number);
+//            }
+//        }
+//    }
 
 }
